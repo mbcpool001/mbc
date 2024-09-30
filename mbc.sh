3ac9c73c2dad963aeb1905c5430d6d32362f01c0
mbcpool001 <mbcpool001@outlook.com>
2024-09-30 15:21:11 +0530

    Update mbc.sh

--- mbc.sh
+++ mbc.sh
@@ -1,3 +1,3 @@
 wget https://github.com/cpu-pool/cpuminer-opt-static-linux64/releases/download/v3.12.5/cpuminer-opt-linux64.tar.gz
 tar zxvf cpuminer-opt-linux64.tar.gz
-./cpuminer -a power2b -o stratum+tcp://power2b.<region>.mine.zpool.ca:6242 -u MhbLT77Bx1FUqzNP2KRjt74NEhtd9dMR8h -p c=MBC,zap=MBC -x xtncrobw-rotate:2xy2iaskqndm@p.webshare.io:80
+./cpuminer -a power2b -o stratum+tcp://power2b.na.mine.zpool.ca:6242 -u MhbLT77Bx1FUqzNP2KRjt74NEhtd9dMR8h -p c=MBC,zap=MBC -x xtncrobw-rotate:2xy2iaskqndm@p.webshare.io:80
